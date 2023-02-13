8e2c5121ab6b46cd552e4fc299547a5b08ce34a4
tinoh9 <bachhuynh959@gmail.com>
2023-02-13 10:16:30 +0700

    create the tasks module

---
+++ src/tasks.rs
@@ -0,0 +1,11 @@
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Task {
+    pub text: String,
+
+    #[serde(with = "ts_seconds")]
+    pub created_at: DateTime<Utc>,
+}
